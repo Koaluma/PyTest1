34153d3e46d5b8e2e04800f9b83e584852636575
Koaluma <eduardo950610@gmail.com>
2026-07-13 14:05:30 -0600

    Lesson 2 added, Lesson 1 changes

--- pytest.py
+++ pytest.py
@@ -1,6 +1,13 @@
-#Strings Values
+#Lesson 1: Printing and variables
 
 name = "Eduardo"
 last_name = "Saldivar"
 age = 30
 print(f"im {name} {last_name} and im {age} years old")
+
+#Lesson 2: Math and updating variables
+
+price = 10
+discount = 5
+total = price - discount
+print(total)
